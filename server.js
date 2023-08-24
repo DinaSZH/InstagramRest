336fe178cc6c2e6f0dd02276c7c6b83f8de1b862
DINASZH <dinasaginbayeva21@gmail.com>
2023-08-25 02:50:43 +0400

    added cors configuration

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const passport = require('passport');
+const cors = require("cors");
 
 
 const app = express();
 
 app.use(logger("dev"));
+app.use(cors())
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
